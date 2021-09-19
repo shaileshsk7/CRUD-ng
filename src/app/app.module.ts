c2d9411494b3eba3bd592fc37ecd64e2e4a5c79e
shaileshsk7 <shaileshkamble771995@gmail.com>
2021-09-19 19:26:16 +0530

    CRUD New ng project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
